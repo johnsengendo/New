90a962b2fc55ce4e7dd145c2384e81f91ef9b3d1
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-06-14 13:25:56 +0200

    Add files via upload

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ About: Simple video streaming client.
 """
 
 import socket
+import time
 
 SERVICE_IP = "10.0.0.12"
 SERVICE_PORT = 8888
@@ -16,10 +17,19 @@ if __name__ == "__main__":
 
     print("Streaming has started...")
 
+    start_time = time.time()
+    counter = 0
+
     while True:
         sock.sendto(data, (SERVICE_IP, SERVICE_PORT))
         buffer, _ = sock.recvfrom(65535)
         if buffer:
-            print("Receiving video stream data...")
-        else:
+            counter += 1
+            print(f"Receiving video stream data... Frame {counter}")
+        
+        # Check if 8 seconds have passed
+        if time.time() - start_time > 8:
             break
+
+    print("Streaming has stopped after 8 seconds.")
+    sock.close()
